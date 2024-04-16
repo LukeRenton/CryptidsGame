baa7a687e7c85db36e535a9019c7dad4750f5823
Luke Renton <luke.renton@icloud.com>
2024-04-16 17:18:07 +0200

    File structure changes - moved server to its own folder

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Define any additional API routes or middleware
 
 // Handle requests to root URL - send the React app
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // Start the server
