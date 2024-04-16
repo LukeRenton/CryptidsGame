db969a2588e6694782fcb3988ef8fcab8ba7b219
Luke Renton <luke.renton@icloud.com>
2024-04-16 16:35:56 +0200

    node config changes

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 8080; // Set the port for your server
 
-// Define a route
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Define any additional API routes or middleware
+
+// Handle requests to root URL - send the React app
 app.get('/', (req, res) => {
-    res.send('Hello, World! This is your Express server.');
-});
-
-app.use(express.static('./client/build'));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,"client", "build", 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
