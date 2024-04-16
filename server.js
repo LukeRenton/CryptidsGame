21e0eedbb6b25438aa677c19f0211230974e8739
Luke Renton <luke.renton@icloud.com>
2024-04-16 16:13:08 +0200

    forced rebase to old version

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001; // Set the port for your server
+const path = require('path');
+const port = process.env.PORT || 8080; // Set the port for your server
 
 // Define a route
 app.get('/', (req, res) => {
     res.send('Hello, World! This is your Express server.');
 });
 
-app.use(express.static('../client/build'));
+app.use(express.static('./client/build'));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname,"client", "build", 'index.html'));
 });
 
 // Start the server
