c125560e47d39ecdf6a19bb812c53997b0a860c1
Luke Renton <luke.renton@icloud.com>
2024-04-26 19:19:52 +0200

    added verbose tile indicators

--- server/controllers/boardController.js
+++ server/controllers/boardController.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+// I opted for verbosity over effeciency here, can write as [white, green, blue] and just append string + use modulo for looping 
+const structures_order_normal = ["white_standing_stone", "green_standing_stone", "blue_standing_stone", "white_shack", "green_shack", "blue_shack"]
+const structures_order_advanced = ["white_standing_stone", "green_standing_stone", "blue_standing_stone", "black_standing_stone", "white_shack", "green_shack", "blue_shack", "black_shack"]
 const deciferMapCode = (mapCode) => {
   const tiles = {};
-  for (let i = 0; i < mapCode.length; i++) {
-    if (i<6){
-      tiles[i+1] = mapCode[i];
-    }
+  for (let i = 0; i < 6; i++) {
+      const tile_num = parseInt(mapCode[i], 16);
+      tiles[i+1] = {
+        "png_num" : mapCode[i],
+        // Have to revert the flipped tiles index
+        "tile_num" : tile_num > 6 ? tile_num - 6 : tile_num,
+      }
+  }
+
+  const pieces = {}
+  for (let i = 6; i < (mapCode.length - 6)/2 ; i+=2){
+
   }
   return tiles;
 
