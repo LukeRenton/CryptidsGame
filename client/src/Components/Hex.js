265e79f98a4d5b4668674802cb1d960e8fc5961e
MitchellNeilson <neilson.mitchell2win@gmail.com>
2024-04-30 00:31:04 +0200

    Added hover effects and started tooltips

--- client/src/Components/Hex.js
+++ client/src/Components/Hex.js
@@ -2,14 +2,38 @@ import React, { useEffect, useState } from 'react'
 import '../Styles/Hex.css'
 
 export default function Hex( props ) {
+
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [tooltipInfo, setTooltipInfo] = useState('test');
+
   const renderPieces = () => {
-    console.log(props.hexNum + ' --> ' + props.pieces);
     return props.pieces.map((piece) => {
       return <img className={'hex-piece hex-'+piece.type} src={piece.image}></img>
     })
   }
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  }
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  }
+
+  // const handleMouseHover = (event) => {
+  //   setMouseX(event.clientX);
+  //   setMouseY(event.clientY);
+  //   console.log('x: '+mouseX);
+  // }
+
   return (
     <div className={'hex hex-'+props.hexNum} >
+        {showTooltip ?  <div className='hex-tooltip' > {/* style={{left: `${mouseX}px`, top: `${mouseY+200}px`}} */}
+                          {tooltipInfo}
+                        </div> : <></>}
+        <div className='hex-hitbox' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></div> {/* onMouseMove={handleMouseHover} */}
         <div className='hex-top-bottom bottom-face'>
             <div className='top'></div>
             <div className='middle'></div>
