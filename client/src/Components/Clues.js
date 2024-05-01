dc3e22003bbd1b5d17e1fd00ba1f592d6126874b
MShilenge <2557606@students.wits.ac.za>
2024-05-01 11:09:09 +0200

    Modified Clues

--- client/src/Components/Clues.js
+++ client/src/Components/Clues.js
@@ -5,14 +5,25 @@ import PlantButton from "./PlantButton";
 
 export default function Clues(props) {
     
+    const [clueVisible, setClueVisible] = useState(false);
+    const [currentClueIndex, setCurrentClueIndex] = useState(0);
 
-
+    const toggleClueVisibility = () => {
+        // Toggle the visibility of the clue
+        setClueVisible((prevVisible) => !prevVisible);
+        // Automatically show the next clue after hiding the current one
+        if (!clueVisible) {
+            setCurrentClueIndex((prevIndex) => (prevIndex + 1) % props.clues.length);
+        }
+    };
 
     return (
         <div className="cards">
             <h2>Clue</h2>
-            <p>{props.clues[0]}</p>
-            <button>Fetch Clue</button>
+            {clueVisible && <p>{`Show clue for player ${props.playerNumber}: ${props.clues[currentClueIndex]}`}</p>}
+            <PlantButton onClick={toggleClueVisibility}>
+                {clueVisible ? 'Hide Clue' : 'Show Clue'}
+            </PlantButton>
         </div>
     );
 }
