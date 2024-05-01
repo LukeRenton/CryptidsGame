efbc0d0103050f0159220d0dbcb6341ce35f1cf4
MitchellNeilson <neilson.mitchell2win@gmail.com>
2024-05-01 12:17:03 +0200

    Finalize clues page

--- client/src/Components/Clues.js
+++ client/src/Components/Clues.js
@@ -7,23 +7,87 @@ export default function Clues(props) {
     
     const [clueVisible, setClueVisible] = useState(false);
     const [currentClueIndex, setCurrentClueIndex] = useState(0);
+    const [showAllClues, setShowAllClues] = useState(false);
+    const [clue, setClue] = useState('');
+    const [verifyClue, setVerifyClue] = useState(false);
+
+    const [showHint, setShowHint] = useState(false);
+    const [verifyShowHint, setVerifyShowHint] = useState(false);
+
+    const handleUpdateClues = () => {
+        console.log('clue is '+clueVisible);
+        if (clueVisible) {
+            setCurrentClueIndex(currentClueIndex+1)
+
+            if (currentClueIndex == 3) {
+                setShowAllClues(true);
+            }
+        }
+    }
 
     const toggleClueVisibility = () => {
         // Toggle the visibility of the clue
-        setClueVisible((prevVisible) => !prevVisible);
         // Automatically show the next clue after hiding the current one
-        if (!clueVisible) {
-            setCurrentClueIndex((prevIndex) => (prevIndex + 1) % props.clues.length);
-        }
+        handleUpdateClues();
+        setClueVisible(!clueVisible);
     };
 
+    const toggleSetClue = (new_clue) => {
+        if (new_clue !== clue) {
+            if (verifyClue) {
+                setClue(new_clue);
+                setVerifyClue(false);
+            } else {
+                setClue('Click again to show clue');
+                setVerifyClue(true);
+            }
+        } else {
+            setClue('');
+            setVerifyClue(false);
+        }
+    }
+
+    const handleShowHint = () => {
+        if (!showHint) {
+            if (verifyShowHint) {
+                setShowHint(true);
+                setVerifyShowHint(false);
+            } else {
+                setVerifyShowHint(true);
+            }
+        } else {
+            setShowHint(false);
+            setVerifyShowHint(false);
+        }
+    }
+
     return (
-        <div className="cards">
-            <h2>Clue</h2>
-            {clueVisible && <p>{`Show clue for player ${props.playerNumber}: ${props.clues[currentClueIndex]}`}</p>}
-            <PlantButton onClick={toggleClueVisibility}>
-                {clueVisible ? 'Hide Clue' : 'Show Clue'}
-            </PlantButton>
-        </div>
+        <>
+            {
+            !showAllClues ? <div className="cards">
+                <h2>Clue</h2>
+                <div>
+                    <p className={"clues-clue-"+(clueVisible ? 'visible' : 'invisible')}>{`${props.clues[currentClueIndex]}`}</p>
+                    <PlantButton onClick={toggleClueVisibility}>
+                        {clueVisible ? 'Hide Clue' : `Show Clue for player ${currentClueIndex + 1}`}
+                    </PlantButton>
+                </div>
+            </div>
+            : <div>
+                <div className="cards">
+                    <h2>Clue</h2>
+                    <div className="clues-all-clues">
+                        <p className={"clues-clue-visible"}>{`${clue}`}</p>
+                        {props.clues.map((this_clue, index) => {
+                        return <PlantButton onClick={() => toggleSetClue(this_clue)}>{clue !== this_clue ? `Show clue for player ${index+1}` : `Hide clue for player ${index+1}`}</PlantButton>
+                        })}
+                    </div>
+                    <h2>Hint</h2>
+                    <p>{!verifyShowHint ? (showHint ? props.hint : '') : 'Click again to show hint. Make sure all players are aware of the hint.'}</p>
+                    <PlantButton onClick={handleShowHint}>{!showHint ? 'Show Hint' : 'Hide Hint'}</PlantButton>
+                </div>
+            </div>
+            }
+        </>
     );
 }
